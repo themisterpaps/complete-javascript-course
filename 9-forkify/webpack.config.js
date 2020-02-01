39ed322fab7ad308b247f669ca381d057eef0d48
Henrique <henriquechigumane@outlook.com>
2020-02-02 00:10:30 +0200

    setup babel

--- 9-forkify/webpack.config.js
+++ 9-forkify/webpack.config.js
@@ -1,9 +1,29 @@
 const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
-    entry:'./src/js/index.js',
+    entry:['babel-polyfill','./src/js/index.js'],
     output:{
-        path:path.resolve(__dirname,'dist/js'),
-        filename:'bundle.js'
+        path:path.resolve(__dirname,'dist'),
+        filename:'js/bundle.js'
     },
-    mode: 'developement'
+    deServer:{
+        contentBase:"./dist "
+    },
+    plugin:{
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template: './src/index.html'
+        })
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            }
+        ]
+    }
 }
